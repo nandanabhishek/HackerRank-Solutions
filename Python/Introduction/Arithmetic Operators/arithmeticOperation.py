e6348b0458f34872580a03b31a51f5d472053188
Abhishek Nandan <abhisheknandan00@gmail.com>
2020-11-16 12:27:22 +0530

    Add files via upload

---
+++ Python/Introduction/Arithmetic Operators/arithmeticOperation.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print((a + b), (a - b), (a * b), sep='\n')
+
+
+""" optional solution :
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
+
+"""
