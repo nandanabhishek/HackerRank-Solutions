947bf967d2c089093f235d2440eb3ee6e16bdfac
Abhishek Nandan <abhisheknandan00@gmail.com>
2020-11-24 12:22:50 +0530

    Create deleteNode.c

---
+++ Problem Solving/Linked Lists/Delete a Node/deleteNode.c
@@ -0,0 +1,31 @@
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+   SinglyLinkedListNode *p,*q;
+
+   if(position == 0) {
+      p=head;
+      head=head->next;
+      free(p);
+    }
+   else {
+      p=head;     
+      q=NULL;
+      for(int i=0; i<position && p; i++){
+          q=p;
+          p=p->next;
+      }
+      q->next=p->next;
+      free(p);
+  }
+  return head;
+}
